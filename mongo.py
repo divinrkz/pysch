56a4eadc1d3779a06abc423bbb5f9adb60756721
Divin Irakiza <irakizadivin@gmail.com>
2021-05-22 10:31:16 +0200

    New: pymongo connection initial setup

---
+++ mongo.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+
+succotash_db = client['succotash_db']
+
+tokens_collection = succotash_db['tokens']
+
+
+rec = {
+    title: 'MongoDB and Python',
+    description: 'MongoDB is no SQL database',
+    tags: ['mongodb', 'database', 'NoSQL'],
+    viewers: 104
+}
+
+# inserting the data in the database
+rec = mydatabase.myTable.insert(record)
--- requirements.rd
+++ requirements.rd
@@ -1,1 +1,2 @@
+pymongo==3.11.4
 schedule==1.1.0
