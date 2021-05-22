715106e318b536f5bf9c6179b713659c36e798fc
Divin Irakiza <irakizadivin@gmail.com>
2021-05-22 20:56:56 +0200

    feat: Jobas and Scheduler synchoranisation @v3

--- db/jobs/jobs.py
+++ db/jobs/jobs.py
@@ -1,15 +1,5 @@
 from db.models.mongo import db
-import datetime
-from db.models.enum import EStatus
-from db.utils.utility import Utility
 from bson.objectid import ObjectId
-
-
-record = {
-    'token': Utility.gen_code(),
-    'expiration_date': datetime.datetime.now() + datetime.timedelta(minutes=5),
-    'status': EStatus.ACTIVE
-}
 
 
 class Jobs:
