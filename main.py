2d9cb654dc64206689fb1e69f98e7f1bdf45334d
Divin Irakiza <irakizadivin@gmail.com>
2021-05-22 10:18:41 +0200

    New: Run enough tests

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def job():
 
 
 schedule.every(10).seconds.do(job)
+schedule.every(10).minutes.do(job)
+schedule.every().hour.do(job)
+schedule.every().day.at("10:30").do(job)
+schedule.every(5).to(10).minutes.do(job)
+schedule.every().monday.do(job)
+schedule.every().wednesday.at("13:15").do(job)
+schedule.every().minutes.at(":17").do(job)
 
 
 while True:
