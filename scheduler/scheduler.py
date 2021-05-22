e9ed22888dde50636d050a87e1737c498d65905b
Divin Irakiza <irakizadivin@gmail.com>
2021-05-22 20:35:03 +0200

    feat: Jobas and Scheduler synchoranisation

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -6,17 +6,20 @@ class Schedule:
     def __init__(self, job, **kwargs):
         self.job = job
         self.args = kwargs
-        print('Scheduler running ....')
+        print('Scheduler running ...')
         self.scheduler(self.fn)
 
     def start(self):
+        print('start')
         while True:
             schedule.run_pending()
             time.sleep(1)
 
     def fn(self):
-        self.job(self.args)
+        print(self.args)
+        self.job(self.args['_collection'], self.args['_prop'], self.args['_val'], self.args['_dict'])
 
     def scheduler(self, job):
-        schedule.every().day.at("10:30").do(job)
+        schedule.every(10).seconds.do(job)
+        # schedule.every().day.at("10:30").do(job)
         self.start()
